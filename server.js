9b8be8c797ae54c3fa71beb36edc30e380796809
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-15 23:22:24 +0200

    update

--- server.js
+++ server.js
@@ -4,40 +4,39 @@ const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 
 // express app
-const app = express();
+const server = express();
 
 // connect to mongodb & listen for requests
 const dbURI = "mongodb+srv://jsmn:jsmn1234@cluster0.qsd2aww.mongodb.net/blog-express-app?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => server.listen(3000))
   .catch(err => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+server.set('view engine', 'ejs');
 
-// middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use((req, res, next) => {
+// middleware & static fileserver.use(express.static('public'));
+server.use(express.urlencoded({ extended: true }));
+server.use(morgan('dev'));
+server.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
 // routes
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   res.redirect('/blogs');
 });
 
-app.get('/about', (req, res) => {
+server.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
 // blog routes
-app.use('/blogs', blogRoutes);
+server.use('/blogs', blogRoutes);
 
 // 404 page
-app.use((req, res) => {
+server.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
